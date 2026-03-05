eeb5c9b1c7a2a378d7aba73b35a5f63920b8f152
SheikhMahmudArman <khan69690020@gmail.com>
2026-03-05 18:41:03 +0600

    add files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';       // your homepage component
+import Homepage from './pages/Homepage'; 
+//const isLoggedIn = false; // change this later based on auth
+
+function App() {
+  return (
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Homepage" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
