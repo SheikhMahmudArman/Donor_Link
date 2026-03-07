b4ca394b6fbe28bbc621dff530f51d6da97413fa
SheikhMahmudArman <khan69690020@gmail.com>
2026-03-07 19:54:00 +0600

    fixed Bug

--- server/index.js
+++ server/index.js
@@ -1,33 +1,29 @@
 const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
+dns.setDefaultResultOrder('ipv4first'); 
+
+
 dns.setServers(['8.8.8.8', '8.8.4.4']);
+const dotenv = require("dotenv");
 
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
+
 
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutesLogin.js");
+const authRoutes = require("./routes/donorRoutes");
+
+
 
 dotenv.config();
 
 const app = express();
 
-// CORS FIRST (most important)
-app.use(cors({
-  origin: "http://localhost:5175",
-  credentials: true
-}));
-
-// middlewares
-app.use(express.json());
-app.use(cookieParser());
-
-// routes
-app.use("/api/auth", authRoutesLogin);
-
 connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
